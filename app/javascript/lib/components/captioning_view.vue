7c826950ba526b4a924f386195933cd69ebfa2d1
Rob Flynn <rob@thingerly.com>
2020-08-09 17:45:47 -0400

    Add some caption block markup and data.

--- app/javascript/lib/components/captioning_view.vue
+++ app/javascript/lib/components/captioning_view.vue
@@ -1,12 +1,47 @@
 <script>
 export default {
+  data() {
+    return {
+      captions: [
+        {
+          id: 1,
+          text: "First Caption"
+        },
+        {
+          id: 2,
+          text: "Second Caption\nhas two lines."
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <template>
   <div class="row captioning-view">
     <div>Left</div>
-    <div>Right</div>
+    <div>
+      <ul class="caption-list">
+        <li v-for="caption in this.captions" :key="caption.id" class="caption-row">
+          <div class="caption">
+            <header>
+              <span class="index">N</span>
+              <div class="buttons">
+                <button type="button">X</button>
+              </div>
+            </header>
+            <div class="caption--content">
+              {{ caption.text }}
+            </div>
+            <footer>
+              <span class="in-time">00:00:00:00</span>
+              <span class="out-time">00:00:00:00</span>
+            </footer>
+
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
