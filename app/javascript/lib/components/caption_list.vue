bb488368caa2b88939e2603b923c548e441dada8
Rob Flynn <rob@thingerly.com>
2020-08-09 18:24:01 -0400

    Extract caption list component.

---
+++ app/javascript/lib/components/caption_list.vue
@@ -0,0 +1,112 @@
+<script>
+export default {
+  props: {
+    classes: {
+      type: String,
+      default: null
+    },
+
+    captions: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
+
+<template>
+  <ul class="caption-list" :classes="classes">
+    <li v-for="(caption, index) in this.captions" :key="caption.id" class="caption-row">
+      <div class="caption">
+        <header>
+          <span class="index">{{ index + 1}}</span>
+          <div class="buttons">
+            <button type="button">X</button>
+          </div>
+        </header>
+        <div class="caption--content">
+          {{ caption.text }}
+        </div>
+        <footer>
+          <span class="in-time">00:00:00:00</span>
+          <span class="out-time">00:00:00:00</span>
+        </footer>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+  $padding: 5px;
+  $radius: $padding;
+  $captionBackground: #777;
+
+  li {
+    margin-bottom: 10px;
+    margin-right: 10px;
+  }
+
+  .caption {
+    background: $captionBackground;
+    border-radius: $radius;
+    padding: $radius * 2;
+
+    header {
+      display: flex;
+      flex-direction: row;
+      margin-bottom: 10px;
+
+      .index {
+        background: skyblue;
+        color: 444;
+        padding: $padding;
+        font-size: 0.9em;
+        width: 15px;
+        text-align: center;
+      }
+
+      .buttons {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        width: 100%;
+
+        button {
+          border: none;
+          background: none;
+          padding: $padding;
+          cursor: pointer;
+        }
+      }
+    }
+
+    &--content {
+      padding: $padding;
+      padding-left: 40px;
+      color: #e5e5e5;
+    }
+
+    footer {
+      margin-top: 10px;
+
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      padding: $padding;
+      padding-left: $padding * 2;
+      padding-right: $padding * 2;
+
+      font-weight: bold;
+      letter-spacing: 2px;
+      font-size: 1.0em;
+
+      .in-time {
+        color: #00ee00;
+      }
+
+      .out-time {
+        color: #df0000;
+      }
+    }
+  }
+</style>
--- app/javascript/lib/components/captioning_view.vue
+++ app/javascript/lib/components/captioning_view.vue
@@ -1,5 +1,11 @@
 <script>
+import CaptionList from "@lib/components/caption_list.vue"
+
 export default {
+  components: {
+    CaptionList
+  },
+
   data() {
     return {
       captions: [
@@ -52,34 +58,13 @@ export default {
       <div>
         Font controls and stuff here
       </div>
-      <ul class="caption-list scroll-y">
-        <li v-for="(caption, index) in this.captions" :key="caption.id" class="caption-row">
-          <div class="caption">
-            <header>
-              <span class="index">{{ index + 1}}</span>
-              <div class="buttons">
-                <button type="button">X</button>
-              </div>
-            </header>
-            <div class="caption--content">
-              {{ caption.text }}
-            </div>
-            <footer>
-              <span class="in-time">00:00:00:00</span>
-              <span class="out-time">00:00:00:00</span>
-            </footer>
-          </div>
-        </li>
-      </ul>
+
+      <CaptionList :captions="captions" />
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-$padding: 5px;
-$radius: $padding;
-$captionBackground: #777;
-
 .captioning-view {
   width: 100%;
 
@@ -92,73 +77,7 @@ $captionBackground: #777;
   }
 
   .caption-list {
-    li {
-      margin: 10px;
-    }
-
-    .caption {
-      background: $captionBackground;
-      border-radius: $radius;
-      padding: $radius * 2;
-
-      header {
-        display: flex;
-        flex-direction: row;
-        margin-bottom: 10px;
-
-        .index {
-          background: skyblue;
-          color: 444;
-          padding: $padding;
-          font-size: 0.9em;
-          width: 15px;
-          text-align: center;
-        }
-
-        .buttons {
-          display: flex;
-          flex-direction: row;
-          justify-content: flex-end;
-          width: 100%;
-
-          button {
-            border: none;
-            background: none;
-            padding: $padding;
-            cursor: pointer;
-          }
-        }
-      }
-
-      &--content {
-        padding: $padding;
-        padding-left: 40px;
-        color: #e5e5e5;
-      }
-
-      footer {
-        margin-top: 10px;
-
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        padding: $padding;
-        padding-left: $padding * 2;
-        padding-right: $padding * 2;
-
-        font-weight: bold;
-        letter-spacing: 2px;
-        font-size: 1.0em;
-
-        .in-time {
-          color: #00ee00;
-        }
-
-        .out-time {
-          color: #df0000;
-        }
-      }
-    }
+    overflow-y: auto;
   }
 }
 </style>
